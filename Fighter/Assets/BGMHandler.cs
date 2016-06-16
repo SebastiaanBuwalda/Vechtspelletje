3e172d549bda5547426d1ec6c75c63d0199ba428
Wopsie <joaghim.beltman@gmail.com>
2016-06-16 23:37:19 +0200

    added background music

---
+++ Fighter/Assets/BGMHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMHandler : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip musicIntro;
+    [SerializeField]
+    private AudioClip musicLoop;
+
+    void Start()
+    {
+        audioSource.PlayOneShot(musicIntro);
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+            audioSource.PlayOneShot(musicLoop);
+    }
+}
