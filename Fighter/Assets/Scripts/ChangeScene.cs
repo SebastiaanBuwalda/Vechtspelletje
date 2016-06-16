723a41c4739f96deca7bb9f740f5f3b44c904518
Wopsie <joaghim.beltman@gmail.com>
2016-06-16 22:39:01 +0200

    added menu & updated enviornment

---
+++ Fighter/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+	
+    //change scene... takes scene index integer as parameter
+	public void ChangeToScene (int sceneToChangeTo)
+	{
+		Application.LoadLevel (sceneToChangeTo);
+	}
+
+    //reload the scene
+    public void ReloadCurrentScene()
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+
+    //quit gimma
+	public void Quit()
+	{
+		Application.Quit();
+		Debug.Log("Quit game");
+	}
+}
